45769df675bc77059151dae5973044d45ef59fd8
Angel Salvador <angelsalvadordev@gmail.com>
2020-03-20 17:58:44 -0500

    funcionalidad creada

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,19 @@
 "use strict";
 
-var animationelements = document.querySelectorAll('[data-scroll-animation]');
-console.log(animationelements);
+var animationElements = Array.from(document.querySelectorAll('[data-scroll-animation]'));
+animationElements.forEach(function (element) {
+  element.classList.add("".concat(element.dataset.scrollAnimation));
+});
+
+var handleScroll = function handleScroll() {
+  var scrollTop = document.documentElement.scrollTop;
+  animationElements.forEach(function (element) {
+    if (element.offsetTop - window.innerHeight / 2 < scrollTop) {
+      element.classList.remove("".concat(element.dataset.scrollAnimation));
+    } else if (element.offsetTop - window.innerHeight > scrollTop) {
+      element.classList.add("".concat(element.dataset.scrollAnimation));
+    }
+  });
+};
+
+window.addEventListener('scroll', handleScroll);
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,30 @@
-let animationelements = document.querySelectorAll('[data-scroll-animation]')
-console.log(animationelements)
+// 1. capturar elementos que tendran animacion
+let animationElements = Array.from(document.querySelectorAll('[data-scroll-animation]'))
+
+// 2. Agregar clases segun el parametro data
+animationElements.forEach(element => {
+  element.classList.add(`${element.dataset.scrollAnimation}`)
+})
+
+const handleScroll = () => {
+
+  // 2. Capturar scroll de usuario
+  let scrollTop = document.documentElement.scrollTop
+
+  // 3. comparar la altura de cada elemento y el scroll para activar la animacion
+  animationElements.forEach(element => {
+    if (element.offsetTop - (window.innerHeight / 2) < scrollTop) {
+      element.classList.remove(`${element.dataset.scrollAnimation}`)
+    } else if (element.offsetTop - window.innerHeight > scrollTop) {
+      element.classList.add(`${element.dataset.scrollAnimation}`)
+    }
+  })
+}
+
+
+// 5. activar comprobacion en scroll
+window.addEventListener('scroll', handleScroll)
+
+
+
+
